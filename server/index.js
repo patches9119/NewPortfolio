a7ba2f5083a22787b0c8c781f8b58ef360ae0e00
unknown <pbsanders1@gmail.com>
2020-12-15 21:11:46 -0800

    updated server file

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
 // app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//    res.sendFile(path.join(publicPath, 'index.html'));
+// });
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
     app.use((req, res) => res.sendFile(path.join(__dirname, "../client/build/index.html")));
