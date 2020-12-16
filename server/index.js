fc73364a8d77a06ffabb285415c7062fa3cfbd48
unknown <pbsanders1@gmail.com>
2020-12-15 18:25:47 -0800

    fixed typo in server file

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 5000;
-app.use(express.static(publicPath));app.get('*', (req, res) => {
+// app.use(express.static(publicPath));
+app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 if (process.env.NODE_ENV === "production") {
